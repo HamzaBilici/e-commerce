c578ea53a404a92f621a39266271e93960b3c3f3
HamzaBilici <hamzablc98@gmail.com>
2026-04-24 20:04:57 +0300

    Toastify+Axios+Icon Library imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { toast } from "react-toastify";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const notify = () => toast("Wow so easy !");
 
   return (
-<>
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button onClick={notify}>Notify !</button>
+    </>
+  );
 }
 
-export default App
+export default App;
